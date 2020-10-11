b7fccee49dfe77892d1973df76df1adeffa66eeb
Jinggo Villamor <jcvillamor@multisyscorp.com>
2020-10-12 06:38:25 +0800

    update instruction

--- instructions.js
+++ instructions.js
@@ -1,13 +1,23 @@
 'use strict'
 
+/**
+ * adonis-amqp-eventbus
+ *
+ * (c) Jinggo Villamor <jcvillamor@multisyscorp.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+*/
+
 const path = require('path')
 
 module.exports = async (cli) => {
   try {
-    const inFile = path.join(__dirname, './config', 'index.js')
-    const outFile = path.join(cli.helpers.configPath(), 'amqp.js')
-    await cli.copy(inFile, outFile)
-    cli.command.completed('create', 'config/amqp.js')
+    await cli.copy(
+      path.join(__dirname, './config/config.js'),
+      path.join(cli.helpers.configPath(), 'eventbus.js')
+    )
+    await cli.command.completed('create', 'config/eventbus.js')
   } catch (error) {
     // ignore error
   }
