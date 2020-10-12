2457773d65341960277981c4777d21bb26e4f4b1
Jinggo Villamor <jcvillamor@multisyscorp.com>
2020-10-12 13:35:14 +0800

    remove command line as cause of the freezing adonis cli

--- providers/AmqpEventBusProvider.js
+++ providers/AmqpEventBusProvider.js
@@ -10,9 +10,10 @@
 const { ServiceProvider } = require('@adonisjs/fold')
 
 const AmqpProducer = require('../src/AmqpProducer');
-const AmqpConsumer = require('../src/AmqpConsumer')
+const AmqpConsumer = require('../src/AmqpConsumer');
 
 class AmqpEventBusProvider extends ServiceProvider {
+  
   register() {
     this.app.singleton('Adonis/Addons/AmqpConsumer', () => {
       const Config = this.app.use('Adonis/Src/Config')
@@ -38,40 +39,7 @@ class AmqpEventBusProvider extends ServiceProvider {
 
     this.app.alias('Adonis/Addons/AmqpProducer', 'AmqpProducer');
     this.app.alias('Adonis/Addons/AmqpConsumer', 'AmqpConsumer');
-    this.app.bind('Adonis/Commands/Publish', () => require('../commands/Publish'));
-    this.app.bind('Adonis/Commands/Consume', () => require('../commands/Consume'));
-  }
-
-  boot() {
-    const ace = require('@adonisjs/ace');
-    ace.addCommand('Adonis/Commands/Publish');
-    ace.addCommand('Adonis/Commands/Consume');
   }
 }
 
 module.exports = AmqpEventBusProvider;
-
-
-
-// class AmqpEventBusProvider extends ServiceProvider {
-//   register() {
-//     this.app.singleton('Adonis/Addons/AmqpEventBus', (app) => {
-//       const Config = app.use('Adonis/Src/Config')
-//       const AmqpEventBus = require('../src/AmqpEventBus')
-
-//       return new AmqpEventBus(Config)
-//     })
-
-//     this.app.alias('Adonis/Addons/AmqpEventBus', 'AmqpEventBus');
-//     this.app.bind('Adonis/Commands/Consume', () => require('../commands/Consume'));
-//     this.app.bind('Adonis/Commands/Publish', () => require('../commands/Publish'))
-//   }
-
-//   boot() {
-//     const ace = require('@adonisjs/ace');
-//     ace.addCommand('Adonis/Commands/Consume');
-//     ace.addCommand('Adonis/Commands/Publish');
-//   }
-// }
-
-// module.exports = AmqpEventBusProvider
