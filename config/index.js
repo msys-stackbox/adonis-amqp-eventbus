296d7a735200bf7fb22ecc60374f94b0060afcd1
Jinggo Villamor <jcvillamor@multisyscorp.com>
2020-10-14 17:17:52 +0800

    Update config template

--- config/index.js
+++ config/index.js
@@ -18,10 +18,22 @@ module.exports = {
   */
   rabbitmq_host: Env.get('RABBITMQ_HOST') || 'localhost',
   rabbitmq_port: Env.get('RABBITMQ_PORT') || '5672',
-  
-  /*
-  * Credentials 
-  */
   rabbitmq_user: Env.get('RABBITMQ_USER') || 'user',
   rabbitmq_password: Env.get('RABBITMQ_PASSWORD') || 'password',
+
+  /*
+  * Consumer to listeners
+  */
+  consumers: [
+    {
+      queueName: 'user-registered',
+      exchange: 'user-exchange',
+      handler: (content) => console.log('From registered %s', content.content.toString())
+    },
+    {
+      queueName: 'user-aggregated',
+      exchange: 'user-exchange',
+      handler: (content) => console.log('From aggregated %s', content.content.toString())
+    }
+  ]
 }
